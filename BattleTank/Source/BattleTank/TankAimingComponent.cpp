2e429f56b6ea869dfd90499e4a355183f91774bf
Sree Nithish B <sreenithishbofficial@gmail.com>
2020-10-25 17:34:00 +0530

    Using FRotators in Unreal

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -45,26 +45,35 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 
 	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity(
+								this,
+								OutLaunchVelocity,
+								StartLocation,
+								HitLocation,
+								LaunchSpeed,
+								ESuggestProjVelocityTraceOption::DoNotTrace
+							);
 
-	if (UGameplayStatics::SuggestProjectileVelocity (
-			this,
-			OutLaunchVelocity,
-			StartLocation,
-			HitLocation,
-			LaunchSpeed,
-			false,
-			0,
-			0,
-			ESuggestProjVelocityTraceOption::DoNotTrace
-			)
-		)
+
+	if (bHaveAimSolution)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		auto TankName = GetOwner()->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("%s Aiming at at %s"), *TankName, *AimDirection.ToString());
+	//	UE_LOG(LogTemp, Warning, TEXT("%s Aiming at at %s"), *TankName, *AimDirection.ToString());
+
+		MoveBarrelTowards(AimDirection);
 	} 
 
 
 
+}
+
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
+{
+	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
+	auto AimAsRotator = AimDirection.Rotation();
+	auto DeltaRotator = AimAsRotator - BarrelRotator;
+
+	UE_LOG(LogTemp, Warning, TEXT("DeltaRotator: %s"), *DeltaRotator.ToString());
 }
 
--- BattleTank/Source/BattleTank/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/TankAimingComponent.h
@@ -30,4 +30,6 @@ public:
 
 private:
 	UStaticMeshComponent* Barrel = nullptr;
+
+	void MoveBarrelTowards(FVector AimDirection);
 };
