8a11fe9b234adee32b31dc379ad2b5c5dc56c959
Sree Nithish B <sreenithishbofficial@gmail.com>
2020-11-11 14:15:15 +0530

    Fixed Track Dissapperaing Bug

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "TankAIController.h"
 #include "Tank.h"
 #include "TankAimingComponent.h"
-#include "TankAIController.h"
+
 
 
 void ATankAIController::BeginPlay()
--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -1,10 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "TankAimingComponent.h"
 #include "TankBarrel.h"
 #include "TankTurret.h"
 #include "Projectile.h"
-#include "TankAimingComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 
